3798846bdbbbb037b7920d3f266bb6f6fe9f6e9a
ProPotam <yotamofri.b@gmail.com>
2025-06-11 16:32:31 +0300

    adding button

--- App/app/(root)/(tabs)/index.tsx
+++ App/app/(root)/(tabs)/index.tsx
@@ -1,10 +1,18 @@
+import { Button } from "@/components/ui/button";
 import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useRouter } from "expo-router"; // Add this import
 
 const AllCalls = () => {
+  const router = useRouter(); // Initialize the router
+
   return (
-    <View>
+    <SafeAreaView>
       <Text>AllCalls</Text>
-    </View>
+      <Button onPress={() => router.push("/sign-in")}>
+        <Text>Go To Log-in</Text>
+      </Button>
+    </SafeAreaView>
   );
 };
 
